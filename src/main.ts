deb9c91cba0a37bb3a22d9801ef56baa054e0447
sinmsinm <alex.balleste@gmail.com>
2020-02-01 16:39:13 +0100

    Adds swagger engine to show up the api examples

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 import 'dotenv/config';
@@ -9,7 +9,20 @@ const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+  .setTitle('JADBack api example')
+  .setDescription('This is the JAD api description')
+  .setVersion('1.0')
+  .addTag('jadback')
+  .build();
+  
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT);
+
+
   Logger.log (`JAD Backend API is listening at ${port}`);
 }
 bootstrap();
