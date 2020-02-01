aa07cc9a53f0b6380fb33da81cb1ab8cf4ecb4cb
Alexandre Ballesté <alex.balleste@gmail.com>
2020-02-01 21:21:57 +0100

    Adds swagger engine to show up the api examples (#19)

--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -1,34 +1,45 @@
 import { IsNotEmpty, IsEmail, IsOptional } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger'
 
 export class UserDTO{
 
+    @ApiProperty()
     @IsNotEmpty()
     username: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     password: string;
 
+    @ApiProperty()
     @IsOptional()
     @IsEmail ()
     email: string;
 
+    @ApiProperty()
     @IsOptional()
     name: string;
 }
 
 export class UserCredentialsDTO {
+
+    @ApiProperty()
     @IsNotEmpty()
     username: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     password: string;
 }
 
 export class UserUpdatableDTO {
+
+    @ApiProperty()
     @IsOptional()
     @IsEmail ()
     email: string;
 
+    @ApiProperty()
     @IsOptional()
     name: string;
 }
